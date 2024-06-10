616636e9c46e9dc1043588a33af6335dce3ee0e2
kykruz <k.kruzan@yahoo.com>
2024-06-10 15:41:47 -0500

    complete before demo

--- TechJobsMVCAutograded6/Controllers/ListController.cs
+++ TechJobsMVCAutograded6/Controllers/ListController.cs
@@ -13,21 +13,24 @@ namespace TechJobsMVCAutograded6.Controllers;
 public class ListController : Controller
 {
     internal static Dictionary<string, string> ColumnChoices = new Dictionary<string, string>()
-        {
-            {"all", "All"},
-            {"employer", "Employer"},
-            {"location", "Location"},
-            {"positionType", "Position Type"},
-            {"coreCompetency", "Skill"}
-        };
-    internal static Dictionary<string, List<JobField>> TableChoices = new Dictionary<string, List<JobField>>()
-        {
-            //{"all", "View All"},
-            {"employer", JobData.GetAllEmployers()},
-            {"location", JobData.GetAllLocations()},
-            {"positionType", JobData.GetAllPositionTypes()},
-            {"coreCompetency", JobData.GetAllCoreCompetencies()}
-        };
+    {
+        { "all", "All" },
+        { "employer", "Employer" },
+        { "location", "Location" },
+        { "positionType", "Position Type" },
+        { "coreCompetency", "Skill" }
+    };
+    internal static Dictionary<string, List<JobField>> TableChoices = new Dictionary<
+        string,
+        List<JobField>
+    >()
+    {
+        //{"all", "View All"},
+        { "employer", JobData.GetAllEmployers() },
+        { "location", JobData.GetAllLocations() },
+        { "positionType", JobData.GetAllPositionTypes() },
+        { "coreCompetency", JobData.GetAllCoreCompetencies() }
+    };
 
     public IActionResult Index()
     {
@@ -44,7 +47,22 @@ public class ListController : Controller
     // TODO #2 - Complete the Jobs action method
     public IActionResult Jobs(string column, string value)
     {
+        List<Job> jobs = new();
+
+        if (value == "View All")
+        {
+            ViewBag.title = "All Jobs";
+            jobs = JobData.FindAll();
+        }
+        
+        else
+        {
+            ViewBag.title = "Jobs including " + column + " of " + value;
+            jobs = JobData.FindByColumnAndValue(column, value);
+        }
+
+        ViewBag.jobs = jobs;
         return View();
     }
+    
 }
-
--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -18,5 +18,27 @@ public class SearchController : Controller
     }
 
     // TODO #3 - Create an action method to process a search request and render the updated search views.
+    public IActionResult Results(string searchType, string searchTerm)
+    {
+        List<Job> jobs = new();
+
+        if (searchType == "all" && string.IsNullOrEmpty(searchTerm))
+        {
+            jobs = JobData.FindAll();
+        }
+        if (searchType == "all")
+        {
+            jobs = JobData.FindByValue(searchTerm);
+        }
+        else
+        {
+            jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+        }
+
+        ViewBag.jobs = jobs;
+        ViewBag.columns = ListController.ColumnChoices;
+
+        return View("Index");
+    }
 }
 
